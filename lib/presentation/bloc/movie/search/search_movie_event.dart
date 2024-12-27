4f48a4b77f8d9ee6f22048fdcb7ebdbc0612ac7c
algokelvin-373 <kelvinht373@gmail.com>
2024-12-27 12:27:14 +0700

    Test Script 1

--- lib/presentation/bloc/movie/search/search_movie_event.dart
+++ lib/presentation/bloc/movie/search/search_movie_event.dart
@@ -2,14 +2,10 @@ part of 'search_movie_bloc.dart';
 
 abstract class SearchMovieEvent extends Equatable {
   const SearchMovieEvent();
-
-  @override
-  List<Object?> get props => [];
 }
 
 class OnQueryMovieChange extends SearchMovieEvent {
   final String query;
-
   OnQueryMovieChange(this.query);
 
   @override
--- lib/presentation/bloc/tv_show/search/search_tv_show_event.dart
+++ lib/presentation/bloc/tv_show/search/search_tv_show_event.dart
@@ -2,14 +2,10 @@ part of 'search_tv_show_bloc.dart';
 
 abstract class SearchTvShowEvent extends Equatable {
   const SearchTvShowEvent();
-
-  @override
-  List<Object?> get props => [];
 }
 
 class OnQueryTvShowChange extends SearchTvShowEvent {
   final String query;
-
   OnQueryTvShowChange(this.query);
 
   @override
