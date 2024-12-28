18c88bb14612b9aaab7c4b18238a975fe4415776
algokelvin-373 <kelvinht373@gmail.com>
2024-12-29 01:42:32 +0700

    Test Script for List Bloc

--- lib/presentation/bloc/tv_show/search/search_tv_show_event.dart
+++ lib/presentation/bloc/tv_show/search/search_tv_show_event.dart
@@ -1,13 +1,10 @@
 part of 'search_tv_show_bloc.dart';
 
-abstract class SearchTvShowEvent extends Equatable {
+abstract class SearchTvShowEvent {
   const SearchTvShowEvent();
 }
 
 class OnQueryTvShowChange extends SearchTvShowEvent {
   final String query;
   OnQueryTvShowChange(this.query);
-
-  @override
-  List<Object?> get props => [query];
 }
